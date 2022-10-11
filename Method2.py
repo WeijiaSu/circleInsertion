c571cf1924a43af4e1a03b03507e52734c604265
weijiasu <weijiasu0804@gmail.com>
2022-10-11 14:57:10 -0400

    update method2

--- Method2.py
+++ Method2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 from Bio import SeqIO
+import numpy as np
+
 
 TE="/data/zhanglab/Weijia_Su/CommonDataSet/TE_full/HMS-Beagle.fasta"
 OutName="171107"
@@ -35,15 +37,110 @@ def Mapping(reads):
 def FilterPaf(paf):
 	f=pd.read_table(paf,header=None)
 	f=f.sort_values([0])
-	f=f[range(9)]
+	f=f[range(11)]
+	r=f.drop_duplicates([0],keep="first")
+	linAli=f.groupby([0]).filter(lambda x: len(x)==1)
+	f=f.loc[~f[0].isin(linAli[0])]
+	f.to_csv(OutName+"_MultiAlig.tsv",header=None,index=None,sep="\t")
+FilterPaf(OutName+"_HMS.paf")
+
+def map_ratio(sub_f):
+	r_len=int(list(sub_f[1])[0])
+	l=zip(sub_f[2],sub_f[3])
+	a=np.array([0]*r_len)
+	for i in l:
+		a[i[0]:i[1]+1]=1
+	return list(a).count(1)
+
+def GetMapping(MulAlig):
+	f=pd.read_table(MulAlig,header=None)
+	f["mLen"]=0
+	for r in set(f[0]):
+		sub_f=f.loc[f[0]==r]
+		p=map_ratio(sub_f)
+		f.loc[f[0]==r,"mLen"]=p
+	f.to_csv(OutName+"_chemReads.tsv",header=None,index=None,sep="\t")
 	print(f[0:10])
 	print(f.shape)
-	r=f.drop_duplicates([0],keep="first")
-	print(r.shape)
-	linAli=f.groupby([0]).filter(lambda x: len(x)==1)
-	print(linAli.shape)
-	print(linAli[0:10])
-	f=f.loc[~f[0].isin(linAli[0])]
-	print(f.shape)
-	print(f.drop_duplicates([0],keep="first").shape)
-FilterPaf(OutName+"_HMS.paf")
+
+#GetMapping(OutName+"_MultiAlig.tsv")
+
+def Junction(list1,list2):
+	n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
+	if n3>n2 and n3-n2>100:
+		return False
+	elif abs(n3-n1)<100 or abs(n2-n4)<100:
+		return False
+	else:
+		return True
+
+
+def isCircle(list1,list2):
+
+	n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
+	t1,t2,t3,t4=list1[2],list1[3],list2[2],list2[3]
+	n_max=max(n1,n2,n3,n4)
+	n_min=min(n1,n2,n3,n4)
+	d1,d2=list1[4],list2[4]
+	t_min=min(t1,t2,t3,t4)
+	t_max=max(t1,t2,t3,t4)
+	j=Junction(list1,list2)
+	readlength=list1[-1]
+	if d1!=d2 or j==False:
+		return False
+	else:
+		if d1=="+" and t_max==t2:
+			return True
+		if d1=="-" and t_max==t4:
+			return True
+		else:
+			return False
+	
+def CircleType(list1,list2):
+
+	n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
+	t1,t2,t3,t4=list1[2],list1[3],list2[2],list2[3]
+	t_max=max(t1,t2,t3,t4)
+	t_min=min(t1,t2,t3,t4)
+	n_max=max(n1,n2,n3,n4)
+	n_min=min(n1,n2,n3,n4)
+	Overlap=n3-n2
+	reflength=list1[-2]
+	readlength=list1[-1]
+	if isCircle(list1,list2) ==False:
+		return "NC"
+	else:
+		return str(t_min)+"_"+str(t_max)+"_"+str(Overlap)
+
+def JunctionReads(chemReads):
+	f=pd.read_table(chemReads,header=None)
+	f[12]=f[1]-f[11]
+	f=f.loc[f[12]>=100]
+	f=f.sort_values([0,2,3])
+	d={}
+	for r in set(f[0]):
+		d[r]="NC"
+		sub=f.loc[f[0]==r]
+		l=list(zip(sub[2],sub[3],sub[7],sub[8],sub[4],sub[6],sub[1]))
+		i=0
+		while i<len(l)-1:
+			list1=l[i]
+			j=i+1
+			while j < len(l):
+				list2=l[j]
+				cirType=CircleType(list1,list2)
+				if cirType!="NC":
+					d[r]=cirType
+					break
+				else:
+					j+=1
+			else:
+				i+=1
+			break
+	f[13]=f[0].apply(lambda x: d[x])
+	f.to_csv(OutName+"_junction.tsv",header=None,index=None,sep="\t")	
+	return f
+	
+#JunctionReads(OutName+"_chemReads.tsv")
+
+
