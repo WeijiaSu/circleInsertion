c7e6d2dc29c7a29bcc172b70b45835e1c848a577
weijiasu <weijiasu0804@gmail.com>
2023-07-16 23:47:37 -0400

    update

--- Method2.py
+++ Method2.py
@@ -16,6 +16,8 @@ OutName=args.OutName
 Jun_type="1LTR_FL"
 reads="/data/zhanglab/Weijia_Su/eccDNA/230619_fly_F2egg_Lig4Aub_gDNA_Tn5/%s.fastq.pre.fastq"%(OutName)
 
+
+Chromosome=["chr2L","chr2R","chr3L","chr3R","chr4","chrX","chrY"]
 def getChimeric_reads(infile):
 	f=pd.read_table(infile)
 	f["0"]=0
@@ -83,8 +85,6 @@ def JunCoor(list1,list2):
 def JunctionReads(mutiAlig):
 	f=pd.read_table(mutiAlig)
 	f["info"]=f["QName"]+"_"+f["RName"]
-	print(f[0:10])
-	print(f.shape)
 	f=f.sort_values(["QName","QStart","QEnd"])
 	d={}
 	for r in set(f["info"]):
@@ -111,11 +111,11 @@ def JunctionReads(mutiAlig):
 	return f
 	
 
-TElengt=7062
 def circleType(x,TElength):
     cirCle_S=int(x.split("_")[0])
     cirCle_E=int(x.split("_")[1])
-    cirCle_D=int(x.split("_")[-1])
+    cirCle_D=int(x.split("_")[2])
+    TElength=int(x.split("_")[3])
     if cirCle_S <100 and cirCle_E>TElengt-100 and cirCle_D<-100:
         return "1LTR_FL"
     elif cirCle_S <100 and cirCle_E>TElengt-100 and cirCle_D>-100:
@@ -130,21 +130,17 @@ def circleType(x,TElength):
 def GetCirType(Junction_reads):
 	f=pd.read_table(Junction_reads,header=None)
 	f=f.loc[f[11]!="NC"]
-	f["Type"]=f[11].apply(lambda x:circleType(x,TElengt))
+	f[12]=f[11]+"_"+f[6].apply(str)
+	f["Type"]=f[12].apply(lambda x:circleType(x))
 	f.to_csv(OutName+"_Type.tsv",index=None,header=None,sep="\t")
 
 def GenomeMaapping(Jun_reads,Jun_type,reads):
 	f=pd.read_table(Jun_reads,header=None)
-	print(f[0:10])
-	print(f.shape)
-
 	f=f.loc[f[12]==Jun_type]
-	print(f)
-	print(f.shape)
 	s=set(f[0])
-	print(len(s))
 	gmap=pd.read_table("%s.fastq_genome.paf"%(OutName),header=None)
 	gmap=gmap.loc[gmap[0].isin(list(f[0]))]
+	gmap=gmap.loc[gmap[5].isin(Chromosome)]
 	gmap.to_csv(OutName+"_"+Jun_type+".Gmap.tsv",header=None,index=None,sep="\t")
 	tmap=pd.read_table("%s.fastq_TE.paf"%(OutName),header=None,sep="\t")
 	tmap=tmap.loc[tmap[0].isin(list(f[0]))]
@@ -171,11 +167,9 @@ def CombineMapping(Gmap,Tmap):
 		max_=sub["rTE_e"].max()
 		combined.loc[combined["rName"]==r,"rTE_min"]=min_
 		combined.loc[combined["rName"]==r,"rTE_max"]=max_
-	print(len(set(combined["rName"])))
 	combined1=combined.loc[(combined["rGenome_e"]<=combined["rTE_min"]+100) | (combined["rGenome_s"]>=combined["rTE_max"]-100)]
 	r2=set(combined1["rName"])
 	combined=combined.loc[combined["rName"].isin(r2)]
-	print(combined[0:20])
 	print(len(set(combined["rName"])))
 	combined.to_csv(OutName+"_"+Jun_type+"_cirIns_filter1.tsv",index=None,sep="\t")
 
@@ -217,8 +211,8 @@ def GetInsertion(CombineFile):
 
 #getChimeric_reads(TEmap)
 #JunctionReads(OutName+"_MultiAlig.tsv")
-GetCirType(OutName+"_junction.tsv")
-GenomeMaapping(OutName+"_Type.tsv",Jun_type,reads)
+#GetCirType(OutName+"_junction.tsv")
+#GenomeMaapping(OutName+"_Type.tsv",Jun_type,reads)
 CombineMapping(OutName+"_"+Jun_type+".Gmap.tsv",OutName+"_"+Jun_type+".Tmap.tsv")
 #GetInsertion(OutName+"_"+Jun_type+"_cirIns_filter1.tsv")
 
